be161c19349751187aa7335b16b409278429ffcd
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-30 19:51:31 -0300

    style: change project name and color scheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ONCut | Encontre, Agende e Transforme seu Estilo!",
-  description: "Descubra as melhores barbearias no ONCut. Agende cortes de cabelo, explore estilos únicos e mergulhe na praticidade de um agendamento simplificado. Sua jornada de estilo começa aqui, no ONCut.",
+  title: "QUICK BARBER | Encontre, Agende e Transforme seu Estilo!",
+  description: "Descubra as melhores barbearias no QUICK BARBER. Agende cortes de cabelo, explore estilos únicos e mergulhe na praticidade de um agendamento simplificado. Sua jornada de estilo começa aqui, no QUICK BARBER.",
 };
 
 export default function RootLayout({
