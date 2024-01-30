9e0ca63a90c9c4d12b2b138065b3cf186296b099
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-30 19:51:41 -0300

    feat: add initial search component

--- app/_sections/search-section.tsx
+++ app/_sections/search-section.tsx
@@ -1,15 +1,20 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "../(home)/_components/search";
 
 const SearchSection = () => {
   return (
-    <section className="flex flex-col py-10 px-5  bg-[url('/capa.png')] bg-cover bg-center bg-no-repeat">
-      <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
-      <p className="text-sm capitalize">
-        {format(new Date(), "EEEE',' dd 'de' MMMM", {
-          locale: ptBR,
-        })}
-      </p>
+    <section className="flex flex-col gap-y-5 bg-[url('/capa.png')] bg-cover bg-center bg-no-repeat px-5 py-10">
+      <div className="flex flex-col">
+        <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+      <Search />
     </section>
   );
 };
