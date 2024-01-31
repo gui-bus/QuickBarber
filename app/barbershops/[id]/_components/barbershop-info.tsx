dcf1dc62e93540ff663866edaca9d2424c323415
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-31 18:20:54 -0300

    feat: add barbershop info component

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,244 @@
+"use client";
+import { Button } from "@/app/_components/ui/button";
+import { ChevronLeftIcon } from "lucide-react";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { PiMapPinFill } from "react-icons/pi";
+import { FaStar } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import Image from "next/image";
+import { Separator } from "@/app/_components/ui/separator";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/_components/ui/tabs";
+import { Barbershop } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+interface BarbershopInfoProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
+  const handleBackClick = () => {
+    router.back();
+  };
+
+  return (
+    <div className="mx-auto flex w-full max-w-7xl flex-col lg:my-5 lg:flex-row">
+      <div className="relative mx-auto h-64 w-full lg:hidden lg:h-96">
+        <Button
+          onClick={handleBackClick}
+          variant={"outline"}
+          className="absolute left-4 top-4 z-50"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Image
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          fill
+          className="object-cover opacity-70"
+        />
+      </div>
+
+      <h1 className="mt-5 text-center text-2xl font-bold md:hidden md:text-3xl">
+        {barbershop.name}
+      </h1>
+
+      <div className="relative flex w-full justify-around">
+        <Image
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="hidden aspect-square h-auto w-1/3 rounded-3xl object-cover opacity-70 lg:block"
+        />
+
+        <Button
+          onClick={handleBackClick}
+          variant={"outline"}
+          className="absolute left-4 top-4 z-50 hidden lg:block"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <div className="flex flex-col gap-y-4 p-5 lg:w-2/3">
+          <div className="flex flex-col items-center justify-between gap-5 lg:flex-row">
+            <div className="flex items-center justify-around gap-2 lg:pl-5">
+              <div>
+                <h1 className="mb-3 hidden text-center text-2xl font-bold md:block md:text-3xl">
+                  {barbershop.name}
+                </h1>
+                <div className="flex flex-col flex-wrap items-center justify-center gap-2 lg:flex-row lg:gap-4">
+                  <p className="line-clamp-1 flex items-center gap-2 truncate text-sm">
+                    <PiMapPinFill size={20} className="text-primary" />{" "}
+                    {barbershop.address}
+                  </p>
+
+                  <p className="flex items-center gap-2">
+                    <FaWhatsapp size={20} className="text-primary" />{" "}
+                    {barbershop.phone}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <Card>
+              <CardContent className="flex items-center justify-center gap-2 p-5">
+                <p className="flex items-center gap-2 text-xl font-bold">
+                  <FaStar size={30} className="text-primary" />
+                  {barbershop.rating.toString()}
+                </p>
+                <p className="line-clamp-1 truncate text-sm">
+                  ({barbershop.ratingQTD.toString()} Avaliações)
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+
+          <Tabs
+            defaultValue="services"
+            className="flex w-full flex-col items-center justify-center lg:hidden"
+          >
+            <TabsList className="my-2">
+              <TabsTrigger value="services">Serviços</TabsTrigger>
+              <TabsTrigger value="info">Informações</TabsTrigger>
+            </TabsList>
+            <TabsContent value="services">
+              Make changes to your account here.
+            </TabsContent>
+            <TabsContent value="info">
+              <div className="mt-5 flex flex-col items-center justify-between gap-5 lg:flex-row">
+                <Card className="lg:w-full">
+                  <CardContent className="flex flex-col gap-5 p-5">
+                    <div className="flex flex-col text-center md:text-start">
+                      <h3 className="text-xl font-bold">Sobre nós</h3>
+                      <Separator className="my-4" />
+                      <p className="text-sm font-light">
+                        {barbershop.description}
+                      </p>
+                    </div>
+
+                    <Separator />
+
+                    <h4 className="text-center text-sm font-light lg:text-start">
+                      Para melhor atendê-los também proporcionamos alguns itens
+                      como:{" "}
+                    </h4>
+                    <div className="mx-auto grid grid-cols-2 gap-x-3 gap-y-3 lg:grid-cols-3">
+                      {barbershop.bonusItems.map(
+                        (bonusItem: string, index: any) => (
+                          <div
+                            className="flex w-full items-center justify-start gap-2 text-sm font-medium"
+                            key={index}
+                          >
+                            <p className="flex items-center gap-2">
+                              <IoMdCheckmarkCircleOutline
+                                size={20}
+                                className="text-primary"
+                              />
+                              {bonusItem}
+                            </p>
+                          </div>
+                        ),
+                      )}
+                    </div>
+                  </CardContent>
+                </Card>
+
+                <div className="flex w-full flex-col gap-5 lg:w-fit">
+                  <Card>
+                    <CardHeader className="flex items-center justify-center text-center">
+                      Horário de Funcionamento
+                    </CardHeader>
+                    <Separator />
+                    <CardContent className="flex flex-col gap-5 p-5">
+                      <div className="flex flex-col gap-y-3">
+                        {barbershop.openings.map(
+                          (booking: string, index: any) => (
+                            <div
+                              className="flex w-full items-center justify-center gap-2 whitespace-nowrap"
+                              key={index}
+                            >
+                              <p className="text-xs">{booking}</p>
+                            </div>
+                          ),
+                        )}
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+
+          <div className="mt-5 hidden flex-col items-center justify-between gap-5 lg:flex lg:flex-row">
+            <Card className="lg:w-full">
+              <CardContent className="flex flex-col gap-5 p-5">
+                <div className="flex flex-col text-center md:text-start">
+                  <h3 className="text-xl font-bold">Sobre nós</h3>
+                  <Separator className="my-4" />
+                  <p className="text-sm font-light">{barbershop.description}</p>
+                </div>
+
+                <Separator />
+
+                <h4 className="text-center text-sm font-light lg:text-start">
+                  Para melhor atendê-los também proporcionamos alguns itens
+                  como:{" "}
+                </h4>
+                <div className="mx-auto grid grid-cols-2 gap-x-3 gap-y-3 lg:grid-cols-3">
+                  {barbershop.bonusItems.map(
+                    (bonusItem: string, index: any) => (
+                      <div
+                        className="flex w-full items-center justify-start gap-2 text-sm font-medium"
+                        key={index}
+                      >
+                        <p className="flex items-center gap-2">
+                          <IoMdCheckmarkCircleOutline
+                            size={20}
+                            className="text-primary"
+                          />
+                          {bonusItem}
+                        </p>
+                      </div>
+                    ),
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+
+            <div className="flex w-full flex-col gap-5 lg:w-fit">
+              <Card>
+                <CardHeader className="flex items-center justify-center text-center">
+                  Horário de Funcionamento
+                </CardHeader>
+                <Separator />
+                <CardContent className="flex flex-col gap-5 p-5">
+                  <div className="flex flex-col gap-y-3">
+                    {barbershop.openings.map((booking: string, index: any) => (
+                      <div
+                        className="flex w-full items-center justify-center gap-2 whitespace-nowrap"
+                        key={index}
+                      >
+                        <p className="text-xs">{booking}</p>
+                      </div>
+                    ))}
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopInfo;
