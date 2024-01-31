fdd6073ba6755775c8625849b88e0390797a26a1
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-31 20:51:48 -0300

    feat: add link redirect on logo click

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,19 +10,22 @@ import {
 } from "./ui/sheet";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="mx-auto flex w-full max-w-7xl items-center justify-around p-5">
-        <Image
-          src="/logo.png"
-          alt="Quick Barber"
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-44 object-cover"
-        />
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Quick Barber"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-44 object-cover"
+          />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
