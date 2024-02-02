3ac342f20b334191b665dcf6f0e204106f1b181e
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-02 20:44:15 -0300

    feat: add revalidate path

--- app/_actions/cancel-booking.ts
+++ app/_actions/cancel-booking.ts
@@ -1,11 +1,15 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { db } from "../_lib/prisma";
 
 export const cancelBooking = async (bookingId: string) => {
-  return await db.booking.delete({
+  await db.booking.delete({
     where: {
       id: bookingId,
     },
   });
+
+  revalidatePath("/");
+  revalidatePath("/bookings");
 };
