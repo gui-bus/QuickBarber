6867daeaa0c47f32c9b39b5acedc3f66a35be083
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-03 20:09:40 -0300

    feat: add header semantic component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -44,169 +44,171 @@ const Header = () => {
   };
 
   return (
-    <Card>
-      <CardContent className="mx-auto flex w-full max-w-7xl items-center justify-around p-3">
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="Quick Barber"
-            height={0}
-            width={0}
-            sizes="100vw"
-            priority
-            className="h-auto w-32 object-cover"
-          />
-        </Link>
+    <header>
+      <Card>
+        <CardContent className="mx-auto flex w-full max-w-7xl items-center justify-around p-3">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="Quick Barber"
+              height={0}
+              width={0}
+              sizes="100vw"
+              priority
+              className="h-auto w-32 object-cover"
+            />
+          </Link>
 
-        <div className="hidden lg:block w-full max-w-xl">
-          <Search />
-        </div>
+          <div className="hidden w-full max-w-xl lg:block">
+            <Search />
+          </div>
 
-        {status === "loading" ? (
-          <ClipLoader color="#3B82F6" size={20} />
-        ) : (
-          <div>
-            <div className="hidden items-center justify-center gap-4 md:flex">
-              {!data?.user && status === "unauthenticated" ? (
-                <Button
-                  onClick={handleLoginClick}
-                  variant={"default"}
-                  className="flex items-center justify-center gap-2 text-white"
-                >
-                  Login <ImEnter size={20} />
-                </Button>
-              ) : (
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <div className="flex cursor-pointer items-center gap-4">
-                      <Avatar className="h-9 w-9 select-none">
-                        <AvatarImage src={data?.user?.image as string} />
-                        <AvatarFallback>
-                          <FaUser />
-                        </AvatarFallback>
-                      </Avatar>
-                      <p className="text-sm">{data?.user?.name}</p>
-                    </div>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel className="text-sm font-light text-muted-foreground">
-                      {data?.user?.email}
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    {data?.user && status === "authenticated" && (
+          {status === "loading" ? (
+            <ClipLoader color="#3B82F6" size={20} />
+          ) : (
+            <div>
+              <div className="hidden items-center justify-center gap-4 md:flex">
+                {!data?.user && status === "unauthenticated" ? (
+                  <Button
+                    onClick={handleLoginClick}
+                    variant={"default"}
+                    className="flex items-center justify-center gap-2 text-white"
+                  >
+                    Login <ImEnter size={20} />
+                  </Button>
+                ) : (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <div className="flex cursor-pointer items-center gap-4">
+                        <Avatar className="h-9 w-9 select-none">
+                          <AvatarImage src={data?.user?.image as string} />
+                          <AvatarFallback>
+                            <FaUser />
+                          </AvatarFallback>
+                        </Avatar>
+                        <p className="text-sm">{data?.user?.name}</p>
+                      </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuLabel className="text-sm font-light text-muted-foreground">
+                        {data?.user?.email}
+                      </DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      {data?.user && status === "authenticated" && (
+                        <DropdownMenuItem>
+                          <Button variant={"default"} asChild>
+                            <Link href="/bookings" className="text-white">
+                              Agendamentos{" "}
+                              <IoCalendarOutline className="ml-2" size={20} />
+                            </Link>
+                          </Button>
+                        </DropdownMenuItem>
+                      )}
                       <DropdownMenuItem>
-                        <Button variant={"default"} asChild>
-                          <Link href="/bookings" className="text-white">
-                            Agendamentos{" "}
-                            <IoCalendarOutline className="ml-2" size={20} />
-                          </Link>
+                        <Button
+                          onClick={handleLogoutClick}
+                          variant={"default"}
+                          className="flex w-full items-center justify-center gap-2 text-white"
+                        >
+                          Logout <ImExit size={20} />
                         </Button>
                       </DropdownMenuItem>
-                    )}
-                    <DropdownMenuItem>
-                      <Button
-                        onClick={handleLogoutClick}
-                        variant={"default"}
-                        className="flex w-full items-center justify-center gap-2 text-white"
-                      >
-                        Logout <ImExit size={20} />
-                      </Button>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              )}
-            </div>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                )}
+              </div>
 
-            <div className="md:hidden">
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button variant={"outline"} size={"icon"}>
-                    <MenuIcon />
-                  </Button>
-                </SheetTrigger>
-                <SheetContent>
-                  <SheetHeader>
-                    <SheetTitle>Menu</SheetTitle>
-                    <SheetDescription className="flex flex-col items-center justify-center">
-                      <Separator className="my-2" />
-                      {status === "authenticated" ? (
-                        <Card className="mt-5 bg-[url('/capa.png')] bg-cover bg-center bg-no-repeat">
-                          <CardHeader className="flex flex-col gap-2">
-                            <div className="flex cursor-pointer items-center gap-4">
-                              <Avatar className="select-none">
-                                <AvatarImage
-                                  src={data?.user?.image as string}
-                                />
-                                <AvatarFallback>
-                                  <FaUser />
-                                </AvatarFallback>
-                              </Avatar>
-                              <p className="text-sm">{data?.user?.name}</p>
-                            </div>
+              <div className="md:hidden">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant={"outline"} size={"icon"}>
+                      <MenuIcon />
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent>
+                    <SheetHeader>
+                      <SheetTitle>Menu</SheetTitle>
+                      <SheetDescription className="flex flex-col items-center justify-center">
+                        <Separator className="my-2" />
+                        {status === "authenticated" ? (
+                          <Card className="mt-5 bg-[url('/capa.png')] bg-cover bg-center bg-no-repeat">
+                            <CardHeader className="flex flex-col gap-2">
+                              <div className="flex cursor-pointer items-center gap-4">
+                                <Avatar className="select-none">
+                                  <AvatarImage
+                                    src={data?.user?.image as string}
+                                  />
+                                  <AvatarFallback>
+                                    <FaUser />
+                                  </AvatarFallback>
+                                </Avatar>
+                                <p className="text-sm">{data?.user?.name}</p>
+                              </div>
 
-                            <p className="text-sm text-muted-foreground">
-                              {data?.user?.email}
-                            </p>
-                          </CardHeader>
-                        </Card>
-                      ) : (
-                        <Button
-                          onClick={handleLoginClick}
-                          variant={"default"}
-                          className="mt-4 flex w-full items-center justify-center gap-2 text-white"
-                        >
-                          Login <ImEnter size={20} />
-                        </Button>
-                      )}
-
-                      <Separator className="my-5" />
-
-                      <div className="flex w-full flex-col gap-2">
-                        <Button
-                          variant={"outline"}
-                          className=" text-white"
-                          asChild
-                        >
-                          <Link
-                            href="/"
-                            className="flex w-full items-center justify-center gap-2"
-                          >
-                            Página Inicial <RiPagesLine size={20} />
-                          </Link>
-                        </Button>
-
-                        <Button
-                          variant={"outline"}
-                          className=" text-white"
-                          asChild
-                        >
-                          <Link
-                            href="/bookings"
-                            className="flex w-full items-center justify-center gap-2"
-                          >
-                            Agendamentos <IoCalendarOutline size={20} />
-                          </Link>
-                        </Button>
-
-                        {status === "authenticated" && (
+                              <p className="text-sm text-muted-foreground">
+                                {data?.user?.email}
+                              </p>
+                            </CardHeader>
+                          </Card>
+                        ) : (
                           <Button
-                            onClick={handleLogoutClick}
-                            variant={"outline"}
-                            className="flex items-center justify-center gap-2 text-white"
+                            onClick={handleLoginClick}
+                            variant={"default"}
+                            className="mt-4 flex w-full items-center justify-center gap-2 text-white"
                           >
-                            Logout <ImExit size={20} />
+                            Login <ImEnter size={20} />
                           </Button>
                         )}
-                      </div>
-                    </SheetDescription>
-                  </SheetHeader>
-                </SheetContent>
-              </Sheet>
+
+                        <Separator className="my-5" />
+
+                        <div className="flex w-full flex-col gap-2">
+                          <Button
+                            variant={"outline"}
+                            className=" text-white"
+                            asChild
+                          >
+                            <Link
+                              href="/"
+                              className="flex w-full items-center justify-center gap-2"
+                            >
+                              Página Inicial <RiPagesLine size={20} />
+                            </Link>
+                          </Button>
+
+                          <Button
+                            variant={"outline"}
+                            className=" text-white"
+                            asChild
+                          >
+                            <Link
+                              href="/bookings"
+                              className="flex w-full items-center justify-center gap-2"
+                            >
+                              Agendamentos <IoCalendarOutline size={20} />
+                            </Link>
+                          </Button>
+
+                          {status === "authenticated" && (
+                            <Button
+                              onClick={handleLogoutClick}
+                              variant={"outline"}
+                              className="flex items-center justify-center gap-2 text-white"
+                            >
+                              Logout <ImExit size={20} />
+                            </Button>
+                          )}
+                        </div>
+                      </SheetDescription>
+                    </SheetHeader>
+                  </SheetContent>
+                </Sheet>
+              </div>
             </div>
-          </div>
-        )}
-      </CardContent>
-    </Card>
+          )}
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
