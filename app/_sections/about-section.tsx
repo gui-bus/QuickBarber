e2988cd6a1b9112e83c25510e7f76e0a667cf018
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-03 20:09:14 -0300

    style: change about section UI

--- app/_sections/about-section.tsx
+++ app/_sections/about-section.tsx
@@ -2,67 +2,77 @@ import Image from "next/image";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { Separator } from "../_components/ui/separator";
 
+interface infoCardProps {
+  title: string;
+  description: string;
+}
+
+const InfoCard = ({ description, title }: infoCardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex items-center rounded-t-xl p-0 py-3 text-center text-xl font-medium text-white">
+        {title}
+      </CardHeader>
+      <Separator />
+      <CardContent className="bg-background">
+        <CardContent className="p-0 pt-5">
+          <p className="text-sm font-light text-white/70 text-center">{description}</p>
+        </CardContent>
+      </CardContent>
+    </Card>
+  );
+};
+
+const cards = [
+  {
+    title: "Agendamento simplificado",
+    description:
+      "Aqui você tem acesso a um processo de agendamento simples e eficiente, permitindo que você reserve seu horário com facilidade.",
+  },
+  {
+    title: "Barbearias de elite",
+    description:
+      "Explore uma seleção das melhores barbearias parceiras. Encontre ambientes profissionais e qualificados para cuidar do seu visual.",
+  },
+  {
+    title: "Explorando estilos",
+    description:
+      "A QUICK BARBER oferece uma ampla gama de barbearias parceiras que você possa explorar e encontrar o estilo que mais combina com você.",
+  },
+];
+
 const AboutSection = () => {
   return (
     <section>
       <Image
-        src="/banner.png"
+        src="/bannerHome.png"
         alt="QUICK BARBER Banner"
         width={0}
         height={0}
-        className="h-auto w-full object-cover"
+        className="h-auto w-full object-cover mb-5"
         sizes="100vw"
       />
 
       <div className="mx-auto w-full max-w-7xl px-5">
-        <div className="flex flex-col items-center justify-center gap-y-4 p-5 text-center">
-          <h1 className="text-3xl font-bold uppercase md:text-4xl">
+        <div className="flex flex-col items-center justify-center gap-y-4 text-center">
+          <h1 className="text-2xl font-bold uppercase md:text-3xl">
             Sua jornada de estilo começa aqui
           </h1>
-          <h2>
-            A QUICK BARBER é uma plataforma de agendamento online para
-            barbearias que simplifica o processo de reserva de horários.
-          </h2>
-
-          <p className="mx-auto w-full max-w-4xl rounded-bl-2xl rounded-tr-2xl border border-primary p-5">
-            Descubra as melhores barbearias, agende diversos tipos de serviços,
-            explore estilos únicos e mergulhe na praticidade de um agendamento
-            simplificado.
+          <p className="text-sm font-light text-white/70">
+            Descubra a praticidade da QUICK BARBER, a plataforma de agendamento
+            online que simplifica todo o processo de reserva de horários em
+            barbearias.
           </p>
         </div>
 
-        <div className="grid grid-cols-1 gap-5 pb-10 pt-5 md:grid-cols-2">
-          <Card>
-            <CardHeader className="flex items-center rounded-t-xl bg-primary text-xl font-bold text-white">
-              Agendamento simplificado
-            </CardHeader>
-            <Separator />
-            <CardContent>
-              <CardContent className="p-5">
-                <p className="text-center text-sm">
-                  Com a QUICK BARBER, você tem acesso a um processo de
-                  agendamento simples e eficiente, permitindo que você reserve
-                  seu horário com facilidade.
-                </p>
-              </CardContent>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader className="flex items-center rounded-t-xl bg-primary text-xl font-bold text-white">
-              Explorando estilos
-            </CardHeader>
-            <Separator />
-            <CardContent>
-              <CardContent className="p-5">
-                <p className="text-center text-sm">
-                  A QUICK BARBER oferece uma ampla gama de barbearias parceiras
-                  que você possa explorar e encontrar o estilo que mais combina
-                  com você.
-                </p>
-              </CardContent>
-            </CardContent>
-          </Card>
+        <div className="hidden grid-cols-1 gap-5 pb-10 pt-5 lg:grid md:grid-cols-2 xl:grid-cols-3">
+          {cards.map((card) => (
+            <InfoCard
+              key={card.title}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
         </div>
       </div>
     </section>
