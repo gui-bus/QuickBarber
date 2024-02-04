fd465c3063de8ba2dd8be936d3dd4ed193d23b30
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-03 21:44:25 -0300

    feat: add message to display that no items where found

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -35,15 +35,21 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
 
   return (
     <div className="mx-auto w-full max-w-7xl p-5">
-      <h1 className="text-center text-xl font-light text-muted-foreground md:text-start">
+      <h1 className="flex items-center gap-2 text-xl font-semibold dark:text-white">
         Exibindo resultados de pesquisa para &quot;{searchParams.search}&quot;
       </h1>
 
-      <div className="mt-5 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
-        {barbershops.map((barbershop: Barbershop) => (
-          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-        ))}
-      </div>
+      {barbershops.length > 0 ? (
+        <div className="mt-5 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+          {barbershops.map((barbershop: Barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      ) : (
+        <div className="mt-5">
+          <p className="text-sm font-light">Oops! Não foi possível encontrar algo com o termo pesquisado.</p>
+        </div>
+      )}
     </div>
   );
 };
